86e1c308e6e8605a47ce8ddc108ee38bede6cc2e
Chantepierre <lucas@lucassifoni.info>
2025-01-16 19:17:46 +0100

    Fixes ios's zoom on inputs

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,68 @@
 <template>
-  <div id="app">
-    <div class="tags">
-      <div class="tag" v-for="route in routes" :class="{'is-primary': route.path === $route.path}">
-        <router-link :to="route.path">{{ route.name }}</router-link>
-      </div>
+    <div id="app">
+        <div class="tags">
+            <div
+                class="tag"
+                v-for="route in routes"
+                :class="{ 'is-primary': route.path === $route.path }"
+            >
+                <router-link :to="route.path">{{ route.name }}</router-link>
+            </div>
+        </div>
+        <div
+            class="box"
+            style="
+                width: 100%;
+                max-width: 540px;
+                margin: auto;
+                margin-bottom: 15px;
+            "
+        >
+            <router-view></router-view>
+        </div>
     </div>
-    <div class="box" style="width: 100%;max-width: 540px;margin:auto;margin-bottom:15px;">
-    <router-view></router-view>
-    </div>
-  </div>
 </template>
 
 <script>
 import "bulma";
 
 export default {
-  computed: {
-    routes() {
-      return this.$router.getRoutes();
+    computed: {
+        routes() {
+            return this.$router.getRoutes();
+        },
     },
-  },
-  mounted() {
-    
-  }
+    mounted() {},
 };
 </script>
 
 <style lang="scss">
-
-html, body {
-  background: gray;
-  overflow-x: hidden;
+html,
+body {
+    background: gray;
+    overflow-x: hidden;
 }
 
 html {
-  font-size: 14.5px!important;
+    font-size: 14.5px !important;
 }
 
 #app {
-  padding: 15px;
+    padding: 15px;
 }
 
 .is-horizontal {
-  display: flex;
-  label {
-    min-width: 10ch;
-  }
+    display: flex;
+    label {
+        min-width: 10ch;
+    }
+}
+
+@media screen and (-webkit-min-device-pixel-ratio: 0) {
+    select,
+    textarea,
+    input {
+        font-size: 16px;
+    }
 }
 </style>
